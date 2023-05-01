f758a2eec956864fa94b9be9e4c43d40df2c05ab
VX3 <40268197+Veeeetzzzz@users.noreply.github.com>
2023-05-01 13:25:41 +0100

    Update postgreSQL.py
    Supports mySQL databases

--- postgreSQL.py
+++ postgreSQL.py
@@ -4,10 +4,19 @@ from langchain.sql_database import SQLDatabase
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
 
+def get_connection_string(db_type, user, password, host, port, database):
+    if db_type.lower() == 'postgresql':
+        return f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}"
+    elif db_type.lower() == 'mysql':
+        return f"mysql+pymysql://{user}:{password}@{host}:{port}/{database}"
+    else:
+        raise ValueError("Unsupported database type. Supported types are 'PostgreSQL' and 'MySQL'")
+
 start_time = time.time()
 
 llm = OpenAI(temperature=0)
 
+db_type = "PostgreSQL"  # Change this to "MySQL" for a MySQL database
 user = "postgres"
 password = "root"
 host = "localhost"
@@ -15,10 +24,11 @@ port = 5432
 database = "test"
 
 try:
-    engine = create_engine(f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}")
-    print("Connected to the database.")
+    connection_string = get_connection_string(db_type, user, password, host, port, database)
+    engine = create_engine(connection_string)
+    print(f"Connected to the {db_type} database.")
 except Exception as e:
-    print(f"Failed to connect to the database: {e}")
+    print(f"Failed to connect to the {db_type} database: {e}")
 
 include_tables=['employees']
 
